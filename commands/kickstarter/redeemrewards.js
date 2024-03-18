403a3087823520a119363c7eeaf867bbce839681
Brandon Clark <bwclark97@gmail.com>
2024-03-18 01:08:25 -0400

    fixed

--- commands/kickstarter/redeemrewards.js
+++ commands/kickstarter/redeemrewards.js
@@ -43,25 +43,26 @@ module.exports = {
                     //Check if provided email exists in sqlite database
                     await user.send('You sent: ' + email);
                     //query database for provided email
-                    db.get(`SELECT * FROM backers WHERE email = ?`, [email], (err, row) => {
+                    db.get(`SELECT * FROM backers WHERE email = ?`, [email], async (err, row) => {
                         if (err) {
                             console.error(err.message);
+                            return;
                         }
                         if (row) {
                             //user is in the database
                             //send user a message with their reward tier
-                            let rewardTier = row.reward_tier;
+                            let rewardTier = row['reward tier'];
+                            console.log(row);
                             let message = `You are a backer with reward tier ${rewardTier}.`;
-                            user.send(message);
+                            await user.send(message);
                             //send user a message with the redeem token
                             let redeemToken = row.redeem_token;
                             message = `Your redeem token is ${redeemToken}. Please use this token to redeem your rewards.`;
-                            user.send(message);
+                            await user.send(message);
                             //send user a message with the instructions to redeem rewards
                             message = `To redeem your rewards, please send this code back PLACEHOLDER`
-                            user.send(message);
-                            const codeFilter = m => m.author.id === user.id; 
-                            const codeCollector = dmChannel.createMessageCollector({ codeFilter, time: 300000 }); // 5 minutes timeout
+                            await user.send(message);
+                            const codeCollector = dmChannel.createMessageCollector({ filter, time: 300000 }); // 5 minutes timeout
                             codeCollector.on('collect', async message => {
                                 const code = message.content;
                                 //trim out whitespace from code
@@ -71,13 +72,14 @@ module.exports = {
                                     //update the database to reflect the redeemed status
                                     db.run(`UPDATE backers SET redeemed = 1 WHERE email = ?`, [email], function(err) {
                                         if (err) {
-                                            return console.error(err.message);
+                                            console.error(err.message);
+                                            return;
                                         }
                                         console.log(`Row(s) updated: ${this.changes}`);
                                     });
                                     //Add relevant roles to the user in the guild
                                     const guild = interaction.guild;
-                                    const member = guild.members.fetch(user.id);
+                                    const member = await guild.members.fetch(user.id);
                                     if (rewardTier === 'Protoshell') {
                                         member.roles.add(protoshellId);
                                     }
@@ -93,19 +95,19 @@ module.exports = {
                                     else if (rewardTier === 'Supporter') {
                                         member.roles.add(supporterId);
                                     }
-                                    user.send('Congratulations! You have successfully redeemed your rewards.');
+                                    await user.send('Congratulations! You have successfully redeemed your rewards.');
+                                    codeCollector.stop();
                                 }
                                 else {
-                                    user.send('Sorry, the code you entered is incorrect. Please try again.');
-                                
-                            }})
+                                    await user.send('Sorry, the code you entered is incorrect. Please try again.');
+                                }
+                            });
                         }
                         else {
                             //user is not in the database
-                            user.send('Sorry, we could not find your email in our database.');
+                            await user.send('Sorry, we could not find your email in our database.');
                         }
                     });
-
                 }
             });
         }
