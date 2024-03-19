5f9608b6a1360db0d5ab0d7542123e21ee8349e6
Brandon Clark <bwclark97@gmail.com>
2024-03-18 22:21:40 -0400

    Done.

--- commands/kickstarter/redeemrewards.js
+++ commands/kickstarter/redeemrewards.js
@@ -1,14 +1,16 @@
 //import discord js
 const { SlashCommandBuilder } = require('discord.js');
 const sqlite3 = require('sqlite3').verbose();
-const {dbName, protoshellId, arikaId, jinsecId, onryoId, supporterId} = require('../../config.json');
+const { dbName, protoshellId, arikaId, jinsecId, onryoId, supporterId, kickstarterRewardsChannel } = require('../../config.json');
+
 //initialize sqlite database
 const db = new sqlite3.Database(dbName, (err) => {
     if (err) {
         console.error(err.message);
     }
     console.log('Connected to the backers database.');
-})
+});
+
 module.exports = {
     data: new SlashCommandBuilder()
         .setName('redeemrewards')
@@ -16,95 +18,108 @@ module.exports = {
     async execute(interaction) {
         const user = interaction.user;
         let email = '';
+
         try {
-            await user.send('Hello! To start the interaction, please provide your email for authentication.');
+            const initialMessage = `
+            **Hello!**
+            <a:hypeglitch:1172174603595026483> Thank you for backing the Kickstarter and helping hit our goal! <a:hypeglitch:1172174603595026483> 
+            
+            To receive your backer roles, please provide the email address you used to back the Kickstarter campaign.
+
+            \`Example\`: \`f4rtWH15P3R@arikacorp.net\`
+            
+            **Please note:**
+            These rewards can only be redeemed on *one* Discord account. Be sure this is the account you want to receieve awards on!`
+            await user.send(initialMessage);
             interaction.reply({ content: 'Please check your DMs for further instructions.', ephemeral: true });
             const dmChannel = await user.createDM(); // Create DM channel if it doesn't exist
             const filter = m => m.author.id === user.id; // Filter for messages from the user
             const collector = dmChannel.createMessageCollector({ filter, time: 30000 }); // 30-second timeout
-    
+
             collector.on('collect', async message => {
                 email = message.content;
                 //remove whitespace from email 
                 email = email.replace(/\s/g, '');
                 collector.stop(); // Stop the collector after receiving the email
-    
-                // Validate and process the email here
-                // ...
-    
+                
                 await message.reply('Received. Processing...');
             });
-    
+
             collector.on('end', async collected => {
                 if (collected.size === 0) {
                     await user.send('You did not respond in time. Please try again.');
                 }
                 else {
-                    //Check if provided email exists in sqlite database
-                    await user.send('You sent: ' + email);
-                    //query database for provided email
+                    // Check if provided email exists in sqlite database
+                    await user.send('Performing ArikaDB <:arika:1171969470982529135> analysis:' + email);
+                    // Query database for provided email
                     db.get(`SELECT * FROM backers WHERE email = ?`, [email], async (err, row) => {
                         if (err) {
                             console.error(err.message);
+                            await user.send('S0m3th1ng has g0n3 c@t@str0ph1c@lly wr0ng. Please try again later.');
                             return;
                         }
                         if (row) {
-                            //user is in the database
-                            //send user a message with their reward tier
+                            // User is in the database
+                            // Send user a message with their reward tier
                             let rewardTier = row['reward tier'];
                             console.log(row);
                             let message = `You are a backer with reward tier ${rewardTier}.`;
                             await user.send(message);
-                            //send user a message with the redeem token
-                            let redeemToken = row.redeem_token;
-                            message = `Your redeem token is ${redeemToken}. Please use this token to redeem your rewards.`;
-                            await user.send(message);
-                            //send user a message with the instructions to redeem rewards
-                            message = `To redeem your rewards, please send this code back PLACEHOLDER`
-                            await user.send(message);
-                            const codeCollector = dmChannel.createMessageCollector({ filter, time: 300000 }); // 5 minutes timeout
-                            codeCollector.on('collect', async message => {
-                                const code = message.content;
-                                //trim out whitespace from code
-                                const trimmedCode = code.replace(/\s/g, '');
-                                if (trimmedCode === redeemToken) {
-                                    //user has redeemed their rewards
-                                    //update the database to reflect the redeemed status
-                                    db.run(`UPDATE backers SET redeemed = 1 WHERE email = ?`, [email], function(err) {
-                                        if (err) {
-                                            console.error(err.message);
-                                            return;
-                                        }
-                                        console.log(`Row(s) updated: ${this.changes}`);
-                                    });
-                                    //Add relevant roles to the user in the guild
-                                    const guild = interaction.guild;
-                                    const member = await guild.members.fetch(user.id);
-                                    if (rewardTier === 'Protoshell') {
-                                        member.roles.add(protoshellId);
-                                    }
-                                    else if (rewardTier === 'Arika') {
-                                        member.roles.add(arikaId);
-                                    }
-                                    else if (rewardTier === 'Platinum') {
-                                        member.roles.add(jinsecId);
-                                    }
-                                    else if (rewardTier === 'Onryo') {
-                                        member.roles.add(onryoId);
-                                    }
-                                    else if (rewardTier === 'Supporter') {
-                                        member.roles.add(supporterId);
-                                    }
-                                    await user.send('Congratulations! You have successfully redeemed your rewards.');
-                                    codeCollector.stop();
+
+                            if (row.redeemed === 1) {
+                                // Reward already redeemed
+                                await user.send('Your rewards have already been redeemed. If this is an error');
+                            } else {
+                                // Reward not redeemed, grant rewards
+                                // Add relevant roles to the user in the guild
+                                const guild = interaction.guild;
+                                const member = await guild.members.fetch(user.id);
+                                
+                                if (rewardTier === 'Proto-Shell') {
+                                    let protoShellAwards = [protoshellId, arikaId, jinsecId, onryoId, supporterId]
+                                    member.roles.add(protoShellAwards)
+                                    .then(()=> console.log('Roles added successfully!'))
+                                    .catch(error => console.error(`Error adding roles: ${error}`))
+                                    
                                 }
-                                else {
-                                    await user.send('Sorry, the code you entered is incorrect. Please try again.');
+                                else if (rewardTier === 'Jin-Sec Loyalist') {
+                                    let jinSecAwards = [jinsecId, supporterId]
+                                    member.roles.add(jinSecAwards)
+                                    .then(()=> console.log('Roles added successfully!'))
+                                    .catch(error => console.error(`Error adding roles: ${error}`))
                                 }
-                            });
+
+                                else if (rewardTier === 'Onryō Insurgent') {
+                                    let onryoAwards = [onryoId, supporterId]
+                                    member.roles.add(onryoAwards)
+                                    .then(()=> console.log('Roles added successfully!'))
+                                    .catch(error => console.error(`Error adding roles: ${error}`))
+                                }
+                                
+                                else if (rewardTier === 'Arika Executive' || rewardTier === 'Arika Overseer') {
+                                    let arikaAwards = [arikaId, onryoId, jinsecId, supporterId]
+                                    member.roles.add(arikaAwards)
+                                    .then(()=> console.log('Roles added successfully!'))
+                                    .catch(error => console.error(`Error adding roles: ${error}`))
+                                }
+                                //Several tiers get the supporter role.
+                                else if (rewardTier === 'Supporter' || rewardTier === 'Prime Supporter' || rewardTier === 'Benefactor' || rewardTier === 'Devout Benefactor') {
+                                    member.roles.add(supporterId);
+                                }
+                                await user.send('Congratulations! You have successfully redeemed your rewards.');
+                                // Update the database to reflect the redeemed status
+                                db.run(`UPDATE backers SET redeemed = 1 WHERE email = ?`, [email], function(err) {
+                                    if (err) {
+                                        console.error(err.message);
+                                        return;
+                                    }
+                                    console.log(`Row(s) updated: ${this.changes}`);
+                                });
+                            }
                         }
                         else {
-                            //user is not in the database
+                            // User is not in the database
                             await user.send('Sorry, we could not find your email in our database.');
                         }
                     });
@@ -113,7 +128,7 @@ module.exports = {
         }
         catch (error) {
             console.error('Could not send message to user.');
-            interaction.reply({ content: 'Could not send message to user.', ephemeral: true})
+            interaction.reply({ content: 'Could not send message to user.', ephemeral: true });
         }
     }
 };
